6ec44b588f8bf04c50137d0312a32eded5282401
liziying-zi <54793507+liziying-zi@users.noreply.github.com>
2019-09-21 11:50:27 +0800

    Update SayHello.java

--- Helloworld/SayHello.java
+++ Helloworld/SayHello.java
@@ -1,13 +1,24 @@
+/**
+*Description:SayHello to my friend
+*Author:     liziying
+*Data:       2019-9-13
+*/
 public class SayHello{
+/**
+*Description:SayHello to my friend
+*Param:      i,sum
+*return:     sum 
+*Date:      2019-9-13
+*/
     public static void main(String[] arge){
-	  System.out.println("Bellow is the function of sayhello");
-	  Sayhello();
+	     System.out.println("Bellow is the function of sayhello");
+	     Sayhello();
    }
     public static void Sayhello(){
 		System.out.println("say hello!"+Sum());
 	}           
     public static int Sum(){
-		int sum=0;
+		int sum=0;//sum 初始化
 		for(int i=1;i<=100;i++){
                sum =sum +i;	
 	}
