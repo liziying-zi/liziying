b3c10f40909f4ccba32dc186f028ec6b3ba3f368
liziying-zi <54793507+liziying-zi@users.noreply.github.com>
2019-09-21 11:44:47 +0800

    Update jing.java

--- Helloworld/jing.java
+++ Helloworld/jing.java
@@ -1,13 +1,24 @@
-import java.util.Scanner;
+/**
+*Description:jing to my friend
+*Author:     liziying
+*Data:       2019-9-16
+*/
+import java.util.Scanner;//导入Scanner类,然后就是创建一个对象:
  
 public class jing 
 {
+	/**
+*Description:jing to my friend
+*Param:      i,j,k,n
+*return:    [NULL]
+*Date:      2019-9-16
+*/
 	public static void main(String[] args) 
 	{
 		int i,j,k,n;
-		Scanner input=new Scanner(System.in);
+		Scanner input=new Scanner(System.in);//扫描从控制台输入的字符
 		System.out.print("input n");
-		n=input.nextInt();
+		n=input.nextInt();//简单文本扫描器
 		for(i=1;i<=n;i++)
 		{
 			for(j=1;j<=n-i;j++)
