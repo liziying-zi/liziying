1377ff9a0938406d713f969f02eb9c145ca67512
liziying-zi <54793507+liziying-zi@users.noreply.github.com>
2019-09-21 11:34:22 +0800

    Update HelloWorld.java

--- Helloworld/HelloWorld.java
+++ Helloworld/HelloWorld.java
@@ -1,7 +1,18 @@
+/**
+*Description:HelloWorld to my friend
+*Author:     liziying
+*Data:       2019-9-13
+*/
 public class HelloWorld{
+/**
+*Description:HelloWorld to my friend
+*Param:     [NULL]
+*return:    [NULL]
+*Date:      2019-9-13
+*/
     public static void main(String[] args){
-	       System.out.println("Hello World");
-           HelloWorld();
+	 System.out.println("Hello World");
+         HelloWorld();
 	}
     public static void HelloWorld(){
 	System.out.println("Hello World");
