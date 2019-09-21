bb0bc65d16b5fb5ce863063ddb295e05977e4604
liziying-zi <54793507+liziying-zi@users.noreply.github.com>
2019-09-21 12:26:14 +0800

    Update Three.java

--- Helloworld/Three.java
+++ Helloworld/Three.java
@@ -1,27 +1,38 @@
+/**
+*Description:Three to my friend
+*Author:     liziying
+*Data:       2019-9-16
+*/
 public class Three{
+/**
+*Description:Three to my friend
+*Param:      res,n1,n2,n3,n4,d,v1,v2
+*return:    [NULL]
+*Date:      2019-9-16
+*/
     public static void main(String[] arge){
-	 int res,n1=12,n2=24,n3=16,n4=3;
-	 double d,v1=10.0,v2=3.1416;
-	 res=n1/n4;
-	 System.out.println(" res=n1/n4="+res);
-	 d=n1/n4;
-	 System.out.println("d=n1/n4="+d);
-     res=n3/n4;
-	 System.out.println(" res=n3/n4="+res);	 
-	 d=n3/n4;
-	 System.out.println("d=n3/n4="+d);
-     d=v1/n4;
-	 System.out.println("d=v1/n4="+d);
-	 d=v1/v2;
-	 System.out.println("d=v1/v2="+d);
-	 res=n1/n2;
-	 System.out.println(" res=n1/n2="+res);	 
-     d=(double)n1/n2;
-	 System.out.println("d=(double)n1/n2="+d);
-     d=n1/(double)n2;
-	 System.out.println("d=n1/(double)n2="+d);
-     d=(double)(n1/n2);
-	 System.out.println("d=(double)(n1/n2)="+d);
-   }
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		res=n1/n4;
+	    System.out.println(" res=n1/n4="+res);
+		d=n1/n4;
+		System.out.println("d=n1/n4="+d);
+        res=n3/n4;
+	    System.out.println(" res=n3/n4="+res);	 
+	    d=n3/n4;
+	    System.out.println("d=n3/n4="+d);
+        d=v1/n4;
+	    System.out.println("d=v1/n4="+d);
+	    d=v1/v2;
+	    System.out.println("d=v1/v2="+d);
+	    res=n1/n2;
+	    System.out.println(" res=n1/n2="+res);	 
+        d=(double)n1/n2;
+	    System.out.println("d=(double)n1/n2="+d);
+        d=n1/(double)n2;
+	    System.out.println("d=n1/(double)n2="+d);
+        d=(double)(n1/n2);
+	    System.out.println("d=(double)(n1/n2)="+d);
+	     }
    }
    
