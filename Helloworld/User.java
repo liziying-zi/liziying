3458e98f4c0ff90c9df344af7b705667c765ee64
Your Name <you@example.com>
2019-10-12 22:56:46 +0800

    created a login

---
+++ Helloworld/User.java
@@ -0,0 +1,33 @@
+/**
+ * @author Administrator
+ * @Date 2019-10-12 
+ * User
+ */
+package com.google.user;
+
+/**
+ * @author Administrator
+ * @Date 2019-10-12 
+ * made by myself
+ */
+public class User {
+	private String username;////获取用户（封装）
+	private String userpassword;//获取用户密码（封装）
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserpassword() {
+		return userpassword;
+	}
+
+	public void setUserpassword(String userpassword) {
+		this.userpassword = userpassword;
+	}
+
+}
